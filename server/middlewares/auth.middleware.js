e55b0f08b6d687585ed1b1a77de450a54a6da9b3
unknown <msarathe705@gmail.com>
2025-06-25 01:22:17 +0530

    first-commit

---
+++ server/middlewares/auth.middleware.js
@@ -0,0 +1,17 @@
+import { asyncHandler } from "../utilities/ayncHandler.utility.js";
+import { errorHandler } from "../utilities/errorHandler.utility.js";
+import jwt from 'jsonwebtoken'
+
+export const isAuthenticated = asyncHandler(async (req, res, next) => {
+
+    const token = req.cookies.token || req.headers['authorization']?.replace("Bearer ","");
+    console.log(req.cookies);
+    
+    if(!token){
+        return next(new errorHandler("Invalid token",400))
+    }
+    const tokenData = jwt.verify(token, process.env.JWT_SECRET)
+    req.user = tokenData
+    next()
+    
+  });
---
+++ server/server.js
@@ -0,0 +1,37 @@
+
+import {app,server} from './Socket/socket.js';
+import dotenv from 'dotenv';
+dotenv.config();
+
+import express from 'express';
+import { connectDB } from './db/connection1.db.js';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+ // Import socket setup
+
+connectDB();
+
+
+app.use(cors({
+    origin: process.env.CLIENT_URL, // Allow requests from the client URL
+    credentials: true, // Allow cookies to be sent
+}))
+app.use(express.json());
+app.use(cookieParser()); 
+
+const PORT = process.env.PORT || 5000
+
+// Routes
+import userRoutes from './routes/user.route.js';
+import messageRoutes from './routes/message.route.js';
+app.use('/api/v1/user', userRoutes);
+app.use('/api/v1/message', messageRoutes);
+
+// Middlewares
+import { errorMiddleware } from './middlewares/error.middleware.js';
+app.use(errorMiddleware);
+
+
+server.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`);
+});
